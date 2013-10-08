24391c06d7e699448ef139de2112631e116013b3
orest <orestx@gmail.com>
2013-10-08 10:55:03 +0300

    in progress

--- js/for-jqueryui.js
+++ js/for-jqueryui.js
@@ -1,9 +1,9 @@
-
+// slider:
 $(function() {
     $( "#slider-range" ).slider({
         range: true,
         min: 0,
-        max: 5000,
+        max: 4000,
         values: [ 275, 900 ],
         slide: function( event, ui ) {
             $( "#amount" ).val( ui.values[ 0 ] +" грн" +  " - " + ui.values[ 1 ] + " грн" );
@@ -12,3 +12,25 @@ $(function() {
 $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) + " грн" +
 " - " + $( "#slider-range" ).slider( "values", 1 ) +  " грн" );
 });
+//end slider
+
+//datepicker:
+$(function() {
+    $( "#from" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+        onClose: function( selectedDate ) {
+            $( "#to" ).datepicker( "option", "minDate", selectedDate );
+        }
+    });
+    $( "#to" ).datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1,
+        onClose: function( selectedDate ) {
+            $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+        }
+    });
+});
+//end datepicker
