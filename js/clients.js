4171841414d86e44342fe612b4732a1e5c851ed5
orest <orestx@gmail.com>
2013-10-15 15:56:49 +0300

    added test clients db

---
+++ js/clients.js
@@ -0,0 +1,14 @@
+var clients = {
+    "id": 1,
+    "fname": "Иван",
+    "lname": "Иванов",
+    "email": "ivanov@mail.ua",
+    "pay_system": ["QIWI", "Webmoney"],
+    "type_of_company": "ЧП",
+    "number_Qiwi": "380436737565",
+    "address": "Мой милый дом",
+    "spep": "49605346",
+    "inn": "439056839478397",
+    "vad": false
+}
+
